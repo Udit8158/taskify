515137ef6ec1220c35861fc548b50dc7e82f5063
Udit8158 <kunduudit8158@gmail.com>
2025-08-06 10:23:59 +0530

    toggle function done - basic level

--- scripts/script.js
+++ scripts/script.js
@@ -171,6 +171,29 @@ const initializeAddTodoEventListener = () => {
   });
 };
 
+const initializeToggleTodosEventListener = () => {
+  const toggleBtns = document.querySelectorAll(".toggleTodos");
+  toggleBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const mainParentEl = btn.parentElement.parentElement;
+      const todoContainer = mainParentEl.children[1]; // hard coded (need to change if DOM changes)
+
+      // check if this category has todos
+      const isEmpty = todoContainer.children.length === 0;
+      if (isEmpty) return; // pass this do nothing
+
+      // if not empty then toggle the class
+      if (!todoContainer.classList.contains("hide")) {
+        todoContainer.classList.add("hide");
+        btn.style.transform = "rotate(90deg)";
+      } else {
+        todoContainer.classList.remove("hide");
+        btn.style.transform = "rotate(0)";
+      }
+    });
+  });
+};
 render(todosState);
 initializeAddTodoEventListener(); // in a static element
 initializeCategoryDropEventListeners(updateCategoryContainer); // in a static element
+initializeToggleTodosEventListener();
